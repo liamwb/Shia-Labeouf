23819e98d2a896c3fcbfb12f8ea5f381855a9034
Liam <liam.woodbaker@gmail.com>
2016-11-16 20:49:20 +1100

    Finished all the conventional attacks and fiddled with classes

---
+++ class.py
@@ -0,0 +1,16 @@
+class bodyparts:
+	def __init___(self, name, dismembered, damageLoss):
+            self.name = name
+            self.dismembered = dismembered
+            self.damageLoss = damageLoss
+
+        leftHand = ‘left hand’, False, -5
+	rightHand = ‘right hand’, False, -5
+	leftArm = ‘left arm’, False, -20
+	rightArm = ‘right arm’, False, -20
+	leftFoot = ‘left foot’, False, -5
+	rightFoot = ‘right foot’, False, -5
+	leftLeg = ‘left leg’, False, -25
+	rightLeg = ‘right leg’, False, -25
+
+            def remove(self, dismembered, damageLoss)
