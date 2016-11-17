c02a8af5436d8632191a4da3726e6fb8bf6e0fb3
Liam <liam.woodbaker@gmail.com>
2016-11-17 20:20:11 +1100

    Did some stuff
    classes are very frustrating and I am confused

--- class.py
+++ class.py
@@ -1,16 +1,16 @@
-class bodyparts:
-	def __init___(self, name, dismembered, damageLoss):
+class bodypart:
+    def __init___(self, name, dismembered, damageLoss):
             self.name = name
             self.dismembered = dismembered
             self.damageLoss = damageLoss
 
-        leftHand = ‘left hand’, False, -5
-	rightHand = ‘right hand’, False, -5
-	leftArm = ‘left arm’, False, -20
-	rightArm = ‘right arm’, False, -20
-	leftFoot = ‘left foot’, False, -5
-	rightFoot = ‘right foot’, False, -5
-	leftLeg = ‘left leg’, False, -25
-	rightLeg = ‘right leg’, False, -25
 
-            def remove(self, dismembered, damageLoss)
+    def remove(self, dismembered, damageLoss):
+        self.dismembered = True
+
+
+leftHand = bodypart()
+leftHand = bodypart.name = ('left hand')
+leftHand = bodypart.dismembered(False)
+leftHand = bodypart.damageLoss(-5)
+
