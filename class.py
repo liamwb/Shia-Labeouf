eccb47569c8ede6f73b8b7f762f72f42255f4302
Liam <liam.woodbaker@gmail.com>
2016-11-18 10:41:21 +1100

    damn extra underscores

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class bodypart:
-    def __init___(self, name, dismembered, damageLoss):
+    def __init__(self, name, dismembered, damageLoss):
             self.name = name
             self.dismembered = dismembered
             self.damageLoss = damageLoss
@@ -9,8 +9,11 @@ class bodypart:
         self.dismembered = True
 
 
-leftHand = bodypart()
-leftHand = bodypart.name = ('left hand')
-leftHand = bodypart.dismembered(False)
-leftHand = bodypart.damageLoss(-5)
-
+leftHand = 'left hand', False, -10
+rightHand = 'right hand', False, -10
+leftArm = 'left arm', False, -22
+rightArm = 'right arm', False, -22
+leftFoot = 'left foot', False, -10
+rightFoot = 'right foot', False, -10
+leftLeg = 'left leg', False, -28
+rightLeg = 'right leg', False, -28
